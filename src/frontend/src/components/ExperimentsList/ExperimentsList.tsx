367604b48cfb2aa059187fb0fb94d04f34c7b56a
pitlis <pitlis95@gmail.com>
2019-12-28 10:42:28 +0300

    frontend: fix formatting experiment results

--- src/frontend/src/components/ExperimentsList/ExperimentsList.tsx
+++ src/frontend/src/components/ExperimentsList/ExperimentsList.tsx
@@ -2,6 +2,7 @@ import React, { PureComponent } from 'react';
 import { AgGridReact } from '@ag-grid-community/react/lib/agGridReact';
 import { AllCommunityModules } from '@ag-grid-community/all-modules';
 import { observer } from 'mobx-react';
+import moment from 'moment';
 
 import './ExperimentsList.scss';
 
@@ -42,37 +43,45 @@ export class ExperimentsList extends PureComponent<IOwnProps> {
                                 headerName: "Название",
                                 field: "name",
                                 sortable: true,
-                                filter: true
+                                filter: true,
+                                suppressMovable: true
                             },
                             {
                                 headerName: "Модель данных",
                                 field: "experimentTemplateName",
                                 sortable: true,
-                                filter: true
+                                filter: true,
+                                suppressMovable: true
                             },
                             {
                                 headerName: "Дата создания",
                                 field: "createdAt",
                                 sortable: true,
-                                filter: true
+                                filter: true,
+                                suppressMovable: true,
+                                valueFormatter: this.dateFormatter
                             },
                             {
                                 headerName: "Текущее состояние",
                                 field: "status",
                                 sortable: true,
                                 filter: true,
-                                valueFormatter: this.statusFormatter
+                                valueFormatter: this.statusFormatter,
+                                suppressMovable: true
                             },
                             {
                                 headerName: "Изменен",
                                 field: "statusChangedAt",
                                 sortable: true,
-                                filter: true
+                                filter: true,
+                                suppressMovable: true,
+                                valueFormatter: this.dateFormatter
                             },
                             {
                                 headerName: "",
                                 sortable: false,
                                 filter: false,
+                                suppressMovable: true,
                                 cellRendererFramework: (event) => {
                                     return <button className='show-results-button'
                                         disabled={event.data.status != ExperimentStatus.Completed}
@@ -94,7 +103,7 @@ export class ExperimentsList extends PureComponent<IOwnProps> {
         );
     }
 
-    private statusFormatter(params: any): string {
+    private statusFormatter(params: { value: number }): string {
         switch (params.value) {
             case 1: {
                 return 'Ожидает обработки';
@@ -112,5 +121,9 @@ export class ExperimentsList extends PureComponent<IOwnProps> {
                 return 'Ошибка обработки';
             }
         }
+    }
+
+    private dateFormatter(params: { value: Date }): string {
+        return moment(params.value).format('DD/MM/YYYY HH:mm')
     }
 }
